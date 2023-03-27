2f203bb78b421d1ce5c8722754682c69fa89c5af
Juan Cruz <juancruzmoran@outlook.es>
2023-03-26 21:43:57 -0300

    java-course

---
+++ src/main/java/activity_1.java
@@ -0,0 +1,16 @@
+
+public class activity_1 {
+
+	public static void main(String[] args) {
+		int numeroInicio = 5;
+		int numeroFin = 14;
+		
+		while (numeroInicio <= numeroFin) {
+			if(numeroInicio %2 == 0) {
+				System.out.println(numeroInicio);
+			}
+			numeroInicio++;
+		}
+	}
+
+}
---
+++ src/main/java/activity_2.java
@@ -0,0 +1,10 @@
+
+public class activity_2 {
+
+	public static void main(String[] args) {
+		String[] separado = "3_tristes tigres_del_13".split("_");
+		System.out.println(separado[1]);
+	}
+
+
+}
